17c46ffbdba2294aa3144b1a47f8bdc63eeaf578
JeongjooLee <jjlee1623@gmail.com>
2023-01-13 15:47:54 +0900

    feat : notion API 적용 테스트

---
+++ pages/api/add-item.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Client } from '@notionhq/client';
+
+const databaseId = process.env.NOTION_DB_ID!;
+const auth = process.env.NOTION_SECRET_KEY;
+
+const notion = new Client({
+  auth,
+});
+
+async function addItem(name: string) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: [
+          {
+            text: {
+              content: name,
+            },
+          },
+        ],
+      },
+    });
+    console.log('RESPONSE :', response);
+  } catch (err) {
+    console.error(`ERROR에요! :${JSON.stringify(err)}`);
+  }
+}
+
+type Data = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { name } = req.query;
+
+  if (name == null) {
+    return res.status(400).json({ message: 'No name' });
+  }
+
+  try {
+    await addItem(String(name));
+    res.status(200).json({ message: `Success ${name} ` });
+  } catch (error) {
+    res.status(500).json({ message: `Faild ${name} ` });
+  }
+}
---
+++ pages/api/get-item.ts
@@ -0,0 +1,45 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Client } from '@notionhq/client';
+
+const databaseId = process.env.NOTION_DB_ID!;
+const auth = process.env.NOTION_SECRET_KEY;
+
+const notion = new Client({
+  auth,
+});
+
+async function getItem() {
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      sorts: [
+        {
+          property: 'price',
+          direction: 'ascending',
+        },
+      ],
+    });
+    console.log('RESPONSE :', response);
+    return response;
+  } catch (err) {
+    console.error(`ERROR에요! :${JSON.stringify(err)}`);
+  }
+}
+
+type Data = {
+  items?: any;
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const response = await getItem();
+    res.status(200).json({ items: response?.results, message: `Success` });
+  } catch (error) {
+    res.status(500).json({ message: `Faild` });
+  }
+}
