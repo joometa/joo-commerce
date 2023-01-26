020230c6b1c21203117a06c56286f1a7a7d4838a
JeongjooLee <jjlee1623@gmail.com>
2023-01-26 21:43:13 +0900

    feat : google 회원가입 구현

---
+++ pages/api/auth/sign-up.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Client } from '@notionhq/client';
+import { PrismaClient } from '@prisma/client';
+import jwtDecode from 'jwt-decode';
+
+interface Decoded {
+  name: string;
+  email: string;
+  picture: string;
+}
+
+const prisma = new PrismaClient();
+
+async function signUp(credential: string) {
+  const decoded: Decoded = jwtDecode(credential);
+
+  try {
+    const response = await prisma.user.upsert({
+      where: {
+        email: decoded.email,
+      },
+      update: {
+        name: decoded.name,
+        image: decoded.picture,
+      },
+      create: {
+        email: decoded.email,
+        name: decoded.name,
+        image: decoded.picture,
+      },
+    });
+    return response;
+  } catch (err) {
+    console.error(`ERROR에요! :${err}`);
+  }
+}
+
+type Data = {
+  items?: any;
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { credential } = req.query;
+
+  try {
+    const token = await signUp(String(credential));
+    res.status(200).json({ items: token, message: `Success` });
+  } catch (error) {
+    res.status(500).json({ message: `Faild` });
+  }
+}
