b2bd735924c4141266fd4802e5458792ea1955f9
JeongjooLee <jjlee1623@gmail.com>
2023-02-03 16:56:10 +0900

    fix : contents 스키마 길이 수정

--- prisma/product-with-category.ts
+++ prisma/product-with-category.ts
@@ -33,10 +33,10 @@ async function main() {
 
   for (const p of productData) {
     // contents 너무길다고 prisma에서 에러나서 잠시 뺌
-    const { contents, ...withoutContent } = p;
+    // const { contents, ...withoutContent } = p;
 
     const product = await prisma.products.create({
-      data: withoutContent,
+      data: p,
     });
     console.log(`Created id : ${product.id}`);
   }
