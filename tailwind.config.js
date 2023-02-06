0bd2756b8d43718d155fec186bd249983783797e
JeongjooLee <jjlee1623@gmail.com>
2023-02-06 18:18:22 +0900

    chore : tailwind rem 단위 변경(pxr)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+const spacingArr = Array.from({ length: 100 }, (_, idx) => idx + 1);
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -6,6 +10,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    spacing: {
+      ...spacingArr.reduce((prev, cur) => {
+        prev[`${cur}pxr`] = pxToRem(cur);
+        return prev;
+      }, {}),
+    },
   },
   plugins: [],
 };
